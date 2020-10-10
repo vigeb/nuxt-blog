cd585d705895df74ed38997f4f7f4b032e177af2
vigeb <reach2101@gmail.com>
2020-10-10 14:11:41 +0700

    add one more table of contents on the article page

--- pages/post/_post.vue
+++ pages/post/_post.vue
@@ -6,8 +6,35 @@
     <img
       :src="doc.cover"
       :alt="doc.title"
-      class="max-w-4/5 border-r-8 border-teal-400 rounded-r-lg shadow-2xl my-4"
+      class="max-w-4/5 border-r-8 border-teal-400 rounded-r-lg shadow-2xl mt-4 mb-8"
     />
+    <div class="w-full mb-4 border border-dashed border-teal-500 p-4">
+      <h3 class="uppercase mb-4 text-center">Mục lục:</h3>
+      <ul>
+        <li v-for="item in doc.toc" :key="item.id" class="my-3">
+          <nuxt-link
+            :to="'#' + item.id"
+            class="inline-block"
+            :class="{
+              'text-teal-500': item.depth === 2,
+              'text-blue-900': item.depth === 3,
+            }"
+          >
+            <span
+              :class="{
+                'ml-2 font-semibold': item.depth === 2,
+                'ml-6 border-l border-teal-400 pl-2 italic': item.depth === 3,
+              }"
+              >{{ item.text }}</span
+            >
+            <div
+              v-if="item.depth === 2"
+              class="border-t w-full border-teal-400"
+            ></div>
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
     <nuxt-content :document="doc" />
     <TableOfContents :items="doc.toc" />
   </div>
