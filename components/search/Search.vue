af38426e973fc61638cf759e337669b2d900f8b2
vigeb <reach2101@gmail.com>
2020-10-12 15:34:36 +0700

    the search cp is ok

--- components/search/Search.vue
+++ components/search/Search.vue
@@ -21,26 +21,29 @@
     </form>
     <div
       v-if="isSearching"
-      class="fixed w-full mt-20 md:absolute md:w-160 md:pt-0 md:pb-0 md:mt-14 top-0 right-0"
+      :class="isHome"
+      class="fixed w-full md:absolute md:w-160 md:pt-0 md:pb-0 md:mt-14 top-0 right-0"
     >
       <div
-        :class="isHome"
         class="bg-white w-full lg:w-160 p-4 rounded-lg shadow-3xl h-full overflow-hidden"
       >
         <div class="w-full h-full">
           <h3 class="text-center uppercase border-b border-gray-300">
             Search results
           </h3>
-          <div class="my-2">
+          <div class="mt-2">
             <Slide>
               <div slot="slide-items" class="whitespace-no-wrap">
-                <label for="all" class="inline-block">
+                <label for="all" class="inline-block mr-1">
                   <input
                     id="all"
+                    v-model="catPath"
                     name="category"
                     type="radio"
+                    value=""
                     class="checkbox hidden"
                     checked
+                    @change="search"
                   />
                   <div
                     class="py-1 px-3 bg-teal-300 rounded-lg text-white text-sm cursor-pointer hover:bg-teal-500 active:bg-teal-500 checked-bg-teal-500"
@@ -48,30 +51,25 @@
                     Tất cả
                   </div>
                 </label>
-                <label for="ca-nhan" class="inline-block">
+                <label
+                  v-for="(cat, index) in categories"
+                  :key="index"
+                  :for="cat.slug"
+                  class="inline-block mr-2"
+                >
                   <input
-                    id="ca-nhan"
+                    :id="cat.slug"
+                    v-model="catPath"
+                    :value="cat.slug"
                     name="category"
                     type="radio"
                     class="checkbox hidden"
+                    @change="search"
                   />
                   <div
                     class="py-1 px-3 text-sm bg-teal-300 rounded-lg text-white cursor-pointer hover:bg-teal-500 active:bg-teal-500 checked-bg-teal-500"
                   >
-                    Cá nhân
-                  </div>
-                </label>
-                <label for="ky-nang-lap-trinh" class="inline-block">
-                  <input
-                    id="ky-nang-lap-trinh"
-                    name="category"
-                    type="radio"
-                    class="checkbox hidden"
-                  />
-                  <div
-                    class="py-1 px-3 text-sm bg-teal-300 rounded-lg text-white cursor-pointer hover:bg-teal-500 active:bg-teal-500 checked-bg-teal-500"
-                  >
-                    kỹ năng lập trình
+                    {{ cat.title }}
                   </div>
                 </label>
               </div>
@@ -99,7 +97,7 @@
           </div>
           <div class="w-full h-full">
             <div
-              class="w-full h-full overflow-y-auto scroll-none max-h-50vh md:scroll-auto"
+              class="w-full h-full overflow-y-auto scroll-none max-h-45vh md:scroll-auto"
             >
               <nuxt-link
                 v-for="(result, index) in results"
@@ -154,12 +152,17 @@ export default {
       query: '',
       isSearching: false,
       results: null,
+      categories: [],
+      catPath: '',
     }
   },
 
   computed: {
     isHome() {
-      return this.isHomePage ? '' : ''
+      return this.isHomePage ? 'mt-32' : 'mt-20'
+    },
+    filter() {
+      return this.catPath === '' ? {} : { catPath: this.catPath }
     },
   },
 
@@ -167,6 +170,14 @@ export default {
     $route() {
       this.isSearching = false
     },
+  },
+
+  async created() {
+    const cats = await this.$content('categories')
+      .only(['title', 'slug'])
+      .fetch()
+
+    this.categories = cats
   },
 
   mounted() {
@@ -177,7 +188,7 @@ export default {
     async search() {
       this.isSearching = true
       const results = await this.$content('articles')
-        .where()
+        .where(this.filter)
         .search(this.query)
         .limit(10)
         .fetch()
