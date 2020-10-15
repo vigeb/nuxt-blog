483da374821743d4d51263c3c78f0e83cd802fe9
vigeb <reach2101@gmail.com>
2020-10-15 21:01:06 +0700

    updated the title ò the website

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import { join } from 'path'
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'src',
+    title: 'vigeb',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
