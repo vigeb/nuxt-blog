6dcd86661d44a0394d2beec6dbaf509113b9774d
vigeb <reach2101@gmail.com>
2020-10-15 20:46:30 +0700

    updated the error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="nuxt-content text-center w-full">
     <svg class="hidden"></svg>
-    <h2 class="mx-4">this is the error page</h2>
+    <h2 class="mx-4">Opps! Lỗi mất tiu rồi!</h2>
+    <blockquote>
+      <p>Có <strong><em>LỖI</em></strong> gì đó ấy nhỉ, cảm phiền bạn <nuxt-link to="/">quay về trang chủ</nuxt-link> tạm nhé!</p>
+    </blockquote>
   </div>
 </template>
